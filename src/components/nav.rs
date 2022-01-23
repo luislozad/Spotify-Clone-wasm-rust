67cce5118353b73bf0ae04de2eab29c30a5657c1
Luis Lozada <luis.lozad@gmail.com>
2022-01-22 20:45:32 -0500

    adding elements

--- src/components/nav.rs
+++ src/components/nav.rs
@@ -1,10 +1,21 @@
 use yew::prelude::*;
 
+use crate::components::prelude::*;
+
 #[function_component(Nav)]
 pub fn nav() -> Html {
+    let ctx = use_context::<AppContext>().expect("no ctx found");
     html! {
-        <div>
-            {"Menu nav"}
-        </div>
+        <ul class={classes!("text-white")}>
+            <li>
+                <span>{ctx.lang.nav_home.clone()}</span>
+            </li>
+            <li>
+                <span>{ctx.lang.nav_search.clone()}</span>
+            </li>
+            <li>
+                <span>{ctx.lang.nav_collection.clone()}</span>
+            </li>
+        </ul>
     }
 }
